8b64e9e2bc868f60f69fb8115f5f45f7d66a180d
Wayne Kenneth Seymour III <WayneSeymour@iheartmedia.com>
2018-01-03 20:16:11 -0500

    Making my way through these again.

---
+++ ch6/exercises.js
@@ -0,0 +1,67 @@
+var _ = require('ramda');
+var accounting = require('accounting');
+
+// Example Data
+var CARS = [{
+  name: 'Ferrari FF',
+  horsepower: 660,
+  dollar_value: 700000,
+  in_stock: true,
+}, {
+  name: 'Spyker C12 Zagato',
+  horsepower: 650,
+  dollar_value: 648000,
+  in_stock: false,
+}, {
+  name: 'Jaguar XKR-S',
+  horsepower: 550,
+  dollar_value: 132000,
+  in_stock: false,
+}, {
+  name: 'Audi R8',
+  horsepower: 525,
+  dollar_value: 114200,
+  in_stock: false,
+}, {
+  name: 'Aston Martin One-77',
+  horsepower: 750,
+  dollar_value: 1850000,
+  in_stock: true,
+}, {
+  name: 'Pagani Huayra',
+  horsepower: 700,
+  dollar_value: 1300000,
+  in_stock: true,
+}];
+
+// Exercise 1:
+// ============
+// Use _.compose() to rewrite the function below. Hint: _.prop() is curried.
+var isLastInStock = function(cars) {
+  var last_car = _.last(cars);
+  return _.prop('in_stock', last_car);
+};
+
+var isLastInStock2 = _.compose(_.prop('in_stock'), _.last)
+// console.log(isLastInStock(CARS))
+// console.log(isLastInStock2(CARS))
+
+// Exercise 2:
+// ============
+// Use _.compose(), _.prop() and _.head() to retrieve the name of the first car.
+var nameOfFirstCar = _.compose(_.prop('name'), _.head)(CARS)
+// console.log(nameOfFirstCar)
+
+// Exercise 3:
+// ============
+// Use the helper function _average to refactor averageDollarValue as a composition.
+var _average = function(xs) {
+  return _.reduce(_.add, 0, xs) / xs.length;
+}; // <- leave be
+
+var averageDollarValue = function(cars) {
+  var dollar_values = _.map(function(c) {
+    return c.dollar_value;
+  }, cars);
+  return _average(dollar_values);
+};
